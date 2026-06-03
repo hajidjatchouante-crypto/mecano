2c10ca3314c275509da9bbfbacf3195d491a50a8
hajidjatchouante-crypto <hajidjatchouante@gmail.com>
2026-06-03 11:28:39 +0200

    chore: remove user-service for rebuild with Docker

--- user-service/src/main/java/com/mecano/userservice/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.mecano.userservice.enums;
-
-public enum Role {
-    ROLE_CLIENT,
-    ROLE_MECANICIEN,
-    ROLE_ADMIN
-}
--- user-service/src/main/java/com/mecano/userservice/enums/Statut.java
+++
@@ -1,7 +0,0 @@
-package com.mecano.userservice.enums;
-
-public enum Statut {
-    ACTIF,
-    INACTIF,
-    SUSPENDU
-}
